05c19abb4068bc2bc9c56610df7b3e7d0c67735a
Aivan Monceller <aivan@monceller.com>
2023-03-11 00:05:13 +0800

    refactor(cleanup): fix prs file checking add format spec as comments

--- LodeRaider/Program.cs
+++ LodeRaider/Program.cs
@@ -1,41 +1,40 @@
-using System;
+﻿using System;
 using System.Globalization;
 using System.Text;
 using System.Text.RegularExpressions;
 
-string dataDir = @"\DATA";
 
-string[] excludeFiles = new string[] { "02.PRD", "09.PRD", "10.PRD", "11.PRD", "12.PRD" };
+string dataDir = Path.Combine(Environment.CurrentDirectory, "DATA");
+string[] excludeFiles = new string[] { };
+
 foreach (string text in Directory.EnumerateFiles(dataDir, "*.PRD"))
 {
     if (!excludeFiles.Contains(text.Substring(text.Length - 6)))
     {
         using (BinaryReader binaryReader = new BinaryReader(File.Open(text, FileMode.Open)))
         {
-            binaryReader.ReadBytes(2);
-            string prsFile = Path.GetFileNameWithoutExtension(text).ToUpperInvariant() + ".PRS";
-            string placeHolder = ToUnicode(binaryReader, 256);
-            string directoryHeader = "RDpcTU9SRUNPREVcTE9ERVJVTk5cREFUQVw=";
-            directoryHeader = Encoding.UTF8.GetString(Convert.FromBase64String(directoryHeader));
-            if (!string.Equals(placeHolder, directoryHeader + prsFile, StringComparison.OrdinalIgnoreCase))
+            binaryReader.ReadBytes(2); // 01PRD skip
+            string prsFilePath = ToUnicode(binaryReader, 256); // 02PRD path to prs file
+            string prsFile = Path.GetFileName(prsFilePath).ToUpperInvariant();
+            string prsPath = Path.Combine(dataDir, prsFile);
+            if (string.IsNullOrEmpty(prsFile) || (!string.IsNullOrEmpty(prsFile) && !File.Exists(prsPath)))
             {
-                Console.WriteLine("Invalid directory: " + text);
+                Console.WriteLine("Invalid resource: " + text);
                 continue;
             }
-            string prsPath = Path.Combine(dataDir, prsFile);
-            binaryReader.ReadBytes(12);
-            short num = binaryReader.ReadInt16();
+            binaryReader.ReadBytes(12); // 03PRD skip
+            short num = binaryReader.ReadInt16(); // 04PRD number of assets
             for (int i = 0; i < (int)num; i++)
             {
-                // Exclude UIX/PZL
-                binaryReader.ReadBytes(10);
+                // asset info
+                binaryReader.ReadBytes(10); // 01AST skip
                 Asset asset = new Asset
                 {
-                    offset = binaryReader.ReadInt32(),
-                    assetType = ToUnicode(binaryReader, 4).ToUpperInvariant(),
-                    id = binaryReader.ReadInt16(),
-                    name = ToUnicode(binaryReader, 18),
-                    length = binaryReader.ReadInt32(),
+                    offset = binaryReader.ReadInt32(), // 02AST offset
+                    assetType = ToUnicode(binaryReader, 4).ToUpperInvariant(), // 03AST type
+                    id = binaryReader.ReadInt16(), // 04AST id
+                    name = ToUnicode(binaryReader, 18), // 05AST name
+                    length = binaryReader.ReadInt32(), // 06AST length
                     prsPath = prsPath
                 };
                 if (asset.length != 0 && asset.offset != 0)
@@ -44,8 +43,8 @@ foreach (string text in Directory.EnumerateFiles(dataDir, "*.PRD"))
                     Console.WriteLine("{0,-20} | {1,-10} | {2,-10} | {3,-10} | {4,-10}", asset, asset.assetType, asset.offset, asset.length, asset.id);
                     // if (asset.assetType == "SND")
                     // {
-                    //     ExtractSound(asset);
-                    //     Console.WriteLine("Extracted sound: " + asset.name);
+                    //    ExtractSound(asset);
+                    //    Console.WriteLine("Extracted sound: " + asset.name);
                     // }
                 }
             }
@@ -53,6 +52,8 @@ foreach (string text in Directory.EnumerateFiles(dataDir, "*.PRD"))
     }
 }
 
+
+// seek inside the PRS file
 void Seek(BinaryReader binaryReader, Asset asset)
 {
     if (binaryReader == null)
@@ -64,9 +65,11 @@ void Seek(BinaryReader binaryReader, Asset asset)
     {
         return;
     }
+    // seek to the offset
     baseStream.Seek((long)asset.offset, SeekOrigin.Begin);
 }
 
+// SND
 void ExtractSound(Asset asset)
 {
     // Open the binary stream
@@ -89,11 +92,11 @@ void ExtractSound(Asset asset)
         byte[] waveData = To16BitPCM(soundData);
 
         // Set the sample rate, number of channels, and bits per sample
-        int sampleRate = 22050; // Modify this to match your game's sample rate
+        int sampleRate = 22050; // Sound files are always 22050 Hz
         int numChannels = 1; // Always use mono for WAV files
         int numBitsPerSample = 16; // Always use 16-bit for WAV files
-        int numBytesPerSample = numChannels * numBitsPerSample / 8;
-        int numBytesPerSecond = sampleRate * numBytesPerSample;
+        int numBytesPerSample = numChannels * numBitsPerSample / 8; // 2 bytes per sample
+        int numBytesPerSecond = sampleRate * numBytesPerSample; // 44100 bytes per second
 
         // Create a new file stream and binary writer
         var fileName = NormalizeFilename(asset);
@@ -102,18 +105,18 @@ void ExtractSound(Asset asset)
         {
             // Write the wave file headers
             binaryWriter.Write("RIFF".ToCharArray());
-            binaryWriter.Write((int)(36 + waveData.Length));
+            binaryWriter.Write((int)(36 + waveData.Length)); // file size , 36 bytes in the header
             binaryWriter.Write("WAVE".ToCharArray());
             binaryWriter.Write("fmt ".ToCharArray());
-            binaryWriter.Write(16);
+            binaryWriter.Write(16); // 16 bytes in the fmt chunk
             binaryWriter.Write((short)1); // PCM audio format
             binaryWriter.Write((short)numChannels);
-            binaryWriter.Write(sampleRate);
-            binaryWriter.Write(numBytesPerSecond);
-            binaryWriter.Write((short)numBytesPerSample);
-            binaryWriter.Write((short)numBitsPerSample);
+            binaryWriter.Write(sampleRate); // sample rate
+            binaryWriter.Write(numBytesPerSecond); // bytes per second
+            binaryWriter.Write((short)numBytesPerSample);  // block align
+            binaryWriter.Write((short)numBitsPerSample); // 16 bits per sample
             binaryWriter.Write("data".ToCharArray());
-            binaryWriter.Write(waveData.Length);
+            binaryWriter.Write(waveData.Length); // data size
 
             // Write the audio data
             binaryWriter.Write(waveData);
@@ -121,21 +124,33 @@ void ExtractSound(Asset asset)
     }
 }
 
-
+// Convert 8-bit PCM to 16-bit PCM
 byte[] To16BitPCM(byte[] data)
 {
-    byte[] array = new byte[data.Length * 2];
+    byte[] array = new byte[data.Length * 2]; // twice the length because 16-bit
     for (int i = 0; i < data.Length; i++)
     {
-        short num = (short)(data[i] - 128 << 8);
-        array[i * 2] = (byte)num;
-        array[i * 2 + 1] = (byte)(num >> 8);
+        // This line takes the current 8-bit sample from the input array and subtracts 128 from it. 
+        // This is because 8-bit audio samples are typically signed, meaning that their range goes from -128 
+        // to +127. By subtracting 128, we center the range around 0
+
+        // The result is then shifted left by 8 bits, effectively multiplying it by 256. 
+        // This is because each 16-bit sample requires two bytes, and there are 8 bits in a byte. 
+        // So shifting left by 8 bits effectively moves the 8-bit sample to the high byte of the 16-bit sample.
+        
+        // This correctly map the range of the 8-bit sample to the larger range of the 16-bit sample  while maintaining the same center point.
+        short num = (short)(data[i] - 128 << 8); // 8-bit to 16-bit
+        // This line stores the low byte of the converted 16-bit sample in the output "array" at the current index multiplied by 2. 
+        // This is because each 16-bit sample requires two bytes, and we are storing the low byte first.
+        array[i * 2] = (byte)num; // low byte
+        // This line stores the high byte of the converted 16-bit sample in the output "array" at the current index multiplied by 2 plus 1. 
+        // This is because each 16-bit sample requires two bytes, and we are storing the high byte second.
+        // The high byte is extracted from the short "num" by shifting it to the right by 8 bits. 
+        // This effectively discards the low byte and keeps only the high byte.
+        array[i * 2 + 1] = (byte)(num >> 8); // high byte
     }
     return array;
 }
-
-//.PRS could stand for "packed resource file" or "compressed resource file," indicating that it contains compressed game data or resources.
-//.PRD could stand for "product data file," indicating that it contains data related to the game product, such as level or character data.
 
 string NormalizeFilename(Asset asset)
 {
@@ -149,13 +164,13 @@ string NormalizeFilename(Asset asset)
 
 string ToUnicode(BinaryReader binaryReader, int length)
 {
-    string text = BitConverter.ToString(binaryReader.ReadBytes(length), 0, length);
-    if (!string.IsNullOrEmpty(text))
+    string hexString = BitConverter.ToString(binaryReader.ReadBytes(length), 0, length);
+    if (!string.IsNullOrEmpty(hexString))
     {
         StringBuilder sb = new StringBuilder();
-        foreach (string substr in text.Split('-'))
+        foreach (string substr in hexString.Split('-'))
         {
-            // Convert each substring to an integer in base 16 (hexadecimal
+            // Convert the hex string to an integer
             int value = Convert.ToInt32(substr, 16);
             if (value != 0)
             {
@@ -165,7 +180,7 @@ string ToUnicode(BinaryReader binaryReader, int length)
         }
         return sb.ToString();
     }
-    return text;
+    return hexString;
 }
 
 
