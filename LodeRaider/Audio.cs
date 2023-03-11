0039ba54d4eceb4530cdd9f55430f9467c2dd813
Aivan Monceller <aivan@monceller.com>
2023-03-11 23:19:51 +0800

    feat(pcm): add msadpcm extract

---
+++ LodeRaider/Audio.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LodeRaider
+{
+    internal class Audio
+    {
+
+        static int[] adaptationTable = new int[]
+        {
+            230, 230, 230, 230, 307, 409, 512, 614,
+            768, 614, 512, 409, 307, 230, 230, 230
+        };
+
+        static int[] adaptationCoeff1 = new int[]
+        {
+            256, 512, 0, 192, 240, 460, 392
+        };
+
+        static int[] adaptationCoeff2 = new int[]
+        {
+            0, -256, 0, 64, 0, -208, -232
+        };
+
+        struct MsAdpcmState
+        {
+            public int delta;
+            public int sample1;
+            public int sample2;
+            public int coeff1;
+            public int coeff2;
+        }
+
+        static int AdpcmMsExpandNibble(ref MsAdpcmState channel, int nibble)
+        {
+            int nibbleSign = nibble - (((nibble & 0x08) != 0) ? 0x10 : 0);
+            int predictor = ((channel.sample1 * channel.coeff1) + (channel.sample2 * channel.coeff2)) / 256 + (nibbleSign * channel.delta);
+
+            if (predictor < -32768)
+                predictor = -32768;
+            else if (predictor > 32767)
+                predictor = 32767;
+
+            channel.sample2 = channel.sample1;
+            channel.sample1 = predictor;
+
+            channel.delta = (adaptationTable[nibble] * channel.delta) / 256;
+            if (channel.delta < 16)
+                channel.delta = 16;
+
+            return predictor;
+        }
+
+        // Convert buffer containing MS-ADPCM wav data to a 16-bit signed PCM buffer
+        internal static byte[] ConvertMsAdpcmToPcm(byte[] buffer, int offset, int count, int channels, int blockAlignment)
+        {
+            MsAdpcmState channel0 = new MsAdpcmState();
+            MsAdpcmState channel1 = new MsAdpcmState();
+            int blockPredictor;
+
+            int sampleCountFullBlock = ((blockAlignment / channels) - 7) * 2 + 2;
+            int sampleCountLastBlock = 0;
+            if ((count % blockAlignment) > 0)
+                sampleCountLastBlock = (((count % blockAlignment) / channels) - 7) * 2 + 2;
+            int sampleCount = ((count / blockAlignment) * sampleCountFullBlock) + sampleCountLastBlock;
+            var samples = new byte[sampleCount * sizeof(short) * channels];
+            int sampleOffset = 0;
+
+            bool stereo = channels == 2;
+
+            while (count > 0)
+            {
+                int blockSize = blockAlignment;
+                if (count < blockSize)
+                    blockSize = count;
+                count -= blockAlignment;
+
+                int totalSamples = ((blockSize / channels) - 7) * 2 + 2;
+                if (totalSamples < 2)
+                    break;
+
+                int offsetStart = offset;
+                blockPredictor = buffer[offset];
+                ++offset;
+                if (blockPredictor > 6)
+                    blockPredictor = 6;
+                channel0.coeff1 = adaptationCoeff1[blockPredictor];
+                channel0.coeff2 = adaptationCoeff2[blockPredictor];
+                if (stereo)
+                {
+                    blockPredictor = buffer[offset];
+                    ++offset;
+                    if (blockPredictor > 6)
+                        blockPredictor = 6;
+                    channel1.coeff1 = adaptationCoeff1[blockPredictor];
+                    channel1.coeff2 = adaptationCoeff2[blockPredictor];
+                }
+
+                channel0.delta = buffer[offset];
+                channel0.delta |= buffer[offset + 1] << 8;
+                if ((channel0.delta & 0x8000) != 0)
+                    channel0.delta -= 0x10000;
+                offset += 2;
+                if (stereo)
+                {
+                    channel1.delta = buffer[offset];
+                    channel1.delta |= buffer[offset + 1] << 8;
+                    if ((channel1.delta & 0x8000) != 0)
+                        channel1.delta -= 0x10000;
+                    offset += 2;
+                }
+
+                channel0.sample1 = buffer[offset];
+                channel0.sample1 |= buffer[offset + 1] << 8;
+                if ((channel0.sample1 & 0x8000) != 0)
+                    channel0.sample1 -= 0x10000;
+                offset += 2;
+                if (stereo)
+                {
+                    channel1.sample1 = buffer[offset];
+                    channel1.sample1 |= buffer[offset + 1] << 8;
+                    if ((channel1.sample1 & 0x8000) != 0)
+                        channel1.sample1 -= 0x10000;
+                    offset += 2;
+                }
+
+                channel0.sample2 = buffer[offset];
+                channel0.sample2 |= buffer[offset + 1] << 8;
+                if ((channel0.sample2 & 0x8000) != 0)
+                    channel0.sample2 -= 0x10000;
+                offset += 2;
+                if (stereo)
+                {
+                    channel1.sample2 = buffer[offset];
+                    channel1.sample2 |= buffer[offset + 1] << 8;
+                    if ((channel1.sample2 & 0x8000) != 0)
+                        channel1.sample2 -= 0x10000;
+                    offset += 2;
+                }
+
+                if (stereo)
+                {
+                    samples[sampleOffset] = (byte)channel0.sample2;
+                    samples[sampleOffset + 1] = (byte)(channel0.sample2 >> 8);
+                    samples[sampleOffset + 2] = (byte)channel1.sample2;
+                    samples[sampleOffset + 3] = (byte)(channel1.sample2 >> 8);
+                    samples[sampleOffset + 4] = (byte)channel0.sample1;
+                    samples[sampleOffset + 5] = (byte)(channel0.sample1 >> 8);
+                    samples[sampleOffset + 6] = (byte)channel1.sample1;
+                    samples[sampleOffset + 7] = (byte)(channel1.sample1 >> 8);
+                    sampleOffset += 8;
+                }
+                else
+                {
+                    samples[sampleOffset] = (byte)channel0.sample2;
+                    samples[sampleOffset + 1] = (byte)(channel0.sample2 >> 8);
+                    samples[sampleOffset + 2] = (byte)channel0.sample1;
+                    samples[sampleOffset + 3] = (byte)(channel0.sample1 >> 8);
+                    sampleOffset += 4;
+                }
+
+                blockSize -= (offset - offsetStart);
+                if (stereo)
+                {
+                    for (int i = 0; i < blockSize; ++i)
+                    {
+                        int nibbles = buffer[offset];
+
+                        int sample = AdpcmMsExpandNibble(ref channel0, nibbles >> 4);
+                        samples[sampleOffset] = (byte)sample;
+                        samples[sampleOffset + 1] = (byte)(sample >> 8);
+
+                        sample = AdpcmMsExpandNibble(ref channel1, nibbles & 0x0f);
+                        samples[sampleOffset + 2] = (byte)sample;
+                        samples[sampleOffset + 3] = (byte)(sample >> 8);
+
+                        sampleOffset += 4;
+                        ++offset;
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < blockSize; ++i)
+                    {
+                        int nibbles = buffer[offset];
+
+                        int sample = AdpcmMsExpandNibble(ref channel0, nibbles >> 4);
+                        samples[sampleOffset] = (byte)sample;
+                        samples[sampleOffset + 1] = (byte)(sample >> 8);
+
+                        sample = AdpcmMsExpandNibble(ref channel0, nibbles & 0x0f);
+                        samples[sampleOffset + 2] = (byte)sample;
+                        samples[sampleOffset + 3] = (byte)(sample >> 8);
+
+                        sampleOffset += 4;
+                        ++offset;
+                    }
+                }
+            }
+
+            return samples;
+        }
+
+        // Convert 8-bit PCM to 16-bit PCM
+        internal static byte[] ConvertPcm8bitTo16bit(byte[] data)
+        {
+            byte[] array = new byte[data.Length * 2]; // twice the length because 16-bit
+            for (int i = 0; i < data.Length; i++)
+            {
+                // This line takes the current 8-bit sample from the input array and subtracts 128 from it. 
+                // This is because 8-bit audio samples are typically signed, meaning that their range goes from -128 
+                // to +127. By subtracting 128, we center the range around 0
+
+                // The result is then shifted left by 8 bits, effectively multiplying it by 256. 
+                // This is because each 16-bit sample requires two bytes, and there are 8 bits in a byte. 
+                // So shifting left by 8 bits effectively moves the 8-bit sample to the high byte of the 16-bit sample.
+
+                // This correctly map the range of the 8-bit sample to the larger range of the 16-bit sample  while maintaining the same center point.
+                short num = (short)(data[i] - 128 << 8); // 8-bit to 16-bit
+                
+                // This line stores the low byte of the converted 16-bit sample in the output "array" at the current index multiplied by 2. 
+                // This is because each 16-bit sample requires two bytes, and we are storing the low byte first.
+                array[i * 2] = (byte)num; // low byte
+                
+                // This line stores the high byte of the converted 16-bit sample in the output "array" at the current index multiplied by 2 plus 1. 
+                // This is because each 16-bit sample requires two bytes, and we are storing the high byte second.
+                // The high byte is extracted from the short "num" by shifting it to the right by 8 bits. 
+                // This effectively discards the low byte and keeps only the high byte.
+                array[i * 2 + 1] = (byte)(num >> 8); // high byte
+            }
+            return array;
+        }
+    }
+}
--- LodeRaider/Program.cs
+++ LodeRaider/Program.cs
@@ -1,57 +1,59 @@
 ﻿using System;
-using System.Globalization;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using LodeRaider;
 
 
 string dataDir = Path.Combine(Environment.CurrentDirectory, "DATA");
-string[] excludeFiles = new string[] { };
 
 foreach (string text in Directory.EnumerateFiles(dataDir, "*.PRD"))
 {
-    if (!excludeFiles.Contains(text.Substring(text.Length - 6)))
+
+    using (BinaryReader binaryReader = new BinaryReader(File.Open(text, FileMode.Open)))
     {
-        using (BinaryReader binaryReader = new BinaryReader(File.Open(text, FileMode.Open)))
+        binaryReader.ReadBytes(2); // 01PRD skip
+        string prsFilePath = ToUnicode(binaryReader, 256); // 02PRD path to prs file
+        string prsFile = Path.GetFileName(prsFilePath).ToUpperInvariant();
+        string prsPath = Path.Combine(dataDir, prsFile);
+        if (string.IsNullOrEmpty(prsFile) || (!string.IsNullOrEmpty(prsFile) && !File.Exists(prsPath)))
         {
-            binaryReader.ReadBytes(2); // 01PRD skip
-            string prsFilePath = ToUnicode(binaryReader, 256); // 02PRD path to prs file
-            string prsFile = Path.GetFileName(prsFilePath).ToUpperInvariant();
-            string prsPath = Path.Combine(dataDir, prsFile);
-            if (string.IsNullOrEmpty(prsFile) || (!string.IsNullOrEmpty(prsFile) && !File.Exists(prsPath)))
+            Console.WriteLine("Invalid resource: " + text);
+            continue;
+        }
+        binaryReader.ReadBytes(12); // 03PRD skip
+        short num = binaryReader.ReadInt16(); // 04PRD number of assets
+        for (int i = 0; i < (int)num; i++)
+        {
+            // asset info
+            binaryReader.ReadBytes(10); // 01AST skip
+            Asset asset = new Asset
             {
-                Console.WriteLine("Invalid resource: " + text);
-                continue;
-            }
-            binaryReader.ReadBytes(12); // 03PRD skip
-            short num = binaryReader.ReadInt16(); // 04PRD number of assets
-            for (int i = 0; i < (int)num; i++)
+                offset = binaryReader.ReadInt32(), // 02AST offset
+                assetType = ToUnicode(binaryReader, 4).ToUpperInvariant(), // 03AST type
+                id = binaryReader.ReadInt16(), // 04AST id
+                name = ToUnicode(binaryReader, 18), // 05AST name
+                length = binaryReader.ReadInt32(), // 06AST length
+                prsPath = prsPath
+            };
+            if (asset.length != 0 && asset.offset != 0)
             {
-                // asset info
-                binaryReader.ReadBytes(10); // 01AST skip
-                Asset asset = new Asset
-                {
-                    offset = binaryReader.ReadInt32(), // 02AST offset
-                    assetType = ToUnicode(binaryReader, 4).ToUpperInvariant(), // 03AST type
-                    id = binaryReader.ReadInt16(), // 04AST id
-                    name = ToUnicode(binaryReader, 18), // 05AST name
-                    length = binaryReader.ReadInt32(), // 06AST length
-                    prsPath = prsPath
-                };
-                if (asset.length != 0 && asset.offset != 0)
-                {
-                    // print name | type | offset | length | id
-                    Console.WriteLine("{0,-20} | {1,-10} | {2,-10} | {3,-10} | {4,-10}", asset, asset.assetType, asset.offset, asset.length, asset.id);
-                    // if (asset.assetType == "SND")
-                    // {
-                    //    ExtractSound(asset);
-                    //    Console.WriteLine("Extracted sound: " + asset.name);
-                    // }
-                }
+                // print name | type | offset | length | id
+                Console.WriteLine("{0,-20} | {1,-10} | {2,-10} | {3,-10} | {4,-10}", asset, asset.assetType, asset.offset, asset.length, asset.id);
+                // if (asset.assetType == "SND" && asset.name == "sierra")
+                // {
+                //     ExtractSound(asset);
+                //     Console.WriteLine("Extracted sound: " + asset.name);
+                // }
+                //if (asset.assetType == "PCM" && asset.name == "CREDITS")
+                //{
+                //    ExtractPCM(asset);
+                //    Console.WriteLine("Extracted pcm: " + asset.name);
+                //}
             }
         }
     }
 }
-
 
 // seek inside the PRS file
 void Seek(BinaryReader binaryReader, Asset asset)
@@ -67,6 +69,113 @@ void Seek(BinaryReader binaryReader, Asset asset)
     }
     // seek to the offset
     baseStream.Seek((long)asset.offset, SeekOrigin.Begin);
+}
+
+void ExtractPCM(Asset asset)
+{
+    // Open the binary stream
+    using (var binaryStream = File.Open(asset.prsPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+    {
+        // Create a binary reader to read the sound data
+        var binaryReader = new BinaryReader(binaryStream);
+
+        // Seek to the data as specified by the asset
+        Seek(binaryReader, asset);
+        var header = Encoding.ASCII.GetString(binaryReader.ReadBytes(4));
+        if (header != "RIFF")
+        {
+            Console.WriteLine($"Not a valid wave file: {asset.name}");
+            return;
+        }
+        binaryReader.ReadInt32();
+        var format = Encoding.ASCII.GetString(binaryReader.ReadBytes(4));
+        if (format != "WAVE")
+        {
+            Console.WriteLine($"Not a valid wave file: {asset.name}");
+            return;
+        }
+
+        WaveData waveData = default(WaveData);
+        while (!(binaryReader == null || binaryReader.BaseStream.Position == (asset.offset + asset.length)))
+        {
+            string section = Encoding.ASCII.GetString(binaryReader.ReadBytes(4)); // fmt desc header
+            int num = binaryReader.ReadInt32(); // size of wave section chunk
+            if (!(section == "fmt "))
+            {
+
+                if (section == "data")
+                {
+
+                    int sampleRate = (int)waveData.Header.SampleRate;
+                    int numChannels = waveData.Header.NumChannels; // Always use mono for WAV files
+                    int numBitsPerSample = 16; // Always use 16-bit for WAV files
+                    int numBytesPerSample = numChannels * numBitsPerSample / 8; // 2 bytes per sample
+                    int numBytesPerSecond = sampleRate * numBytesPerSample;
+
+                    Console.WriteLine("write data");
+                    var fileName = NormalizeFilename(asset);
+                    using (FileStream fileStream = new FileStream($"{fileName}.wav", FileMode.Create))
+                    using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
+                    {
+
+                        // Write the wave file headers
+                        binaryWriter.Write("RIFF".ToCharArray());
+                        binaryWriter.Write((int)(36 + num)); // file size , 36 bytes in the header
+                        binaryWriter.Write("WAVE".ToCharArray());
+                        binaryWriter.Write("fmt ".ToCharArray());
+                        binaryWriter.Write(16); // 16 bytes in the fmt chunk
+                        binaryWriter.Write((short)1); // PCM audio format
+                        binaryWriter.Write((short)numChannels);
+                        binaryWriter.Write(sampleRate); // sample rate
+                        binaryWriter.Write(numBytesPerSecond); // bytes per second
+                        binaryWriter.Write((short)numBytesPerSample);  // block align
+                        binaryWriter.Write((short)numBitsPerSample); // 16 bits per sample
+                        binaryWriter.Write("data".ToCharArray());
+                        binaryWriter.Write(num); // data size
+
+                        byte[] soundData = binaryReader.ReadBytes(num);
+
+                        byte[] pcmData = Audio.ConvertMsAdpcmToPcm(soundData, 0, num, (short)waveData.Header.NumChannels, (short)waveData.Header.BlockAlign);
+
+                        // Write the audio data
+                        binaryWriter.Write(pcmData);
+
+
+                    }
+                    return;
+                }
+                binaryReader.BaseStream.Seek((long)num, SeekOrigin.Current);
+            }
+            else
+            {
+                waveData.Header = new WaveHeader
+                {
+                    FormatType = binaryReader.ReadUInt16(),
+                    NumChannels = binaryReader.ReadUInt16(),
+                    SampleRate = binaryReader.ReadUInt32(),
+                    ByteRate = binaryReader.ReadUInt32(),
+                    BlockAlign = binaryReader.ReadUInt16(),
+                    BitsPerSample = binaryReader.ReadUInt16(),
+                    CbSize = binaryReader.ReadUInt16()
+                };
+                waveData.DataSize = binaryReader.ReadUInt16();
+                waveData.NumSamples = binaryReader.ReadUInt16();
+                waveData.Samples = new WaveSample[7]; // 7 channels max
+                for (int i = 0; i < (int)waveData.NumSamples; i++)
+                {
+                    waveData.Samples[i].LeftChannel = binaryReader.ReadInt16();
+                    waveData.Samples[i].RightChannel = binaryReader.ReadInt16();
+                }
+                if (waveData.Header.FormatType != 2 || waveData.Header.BitsPerSample != 4)
+                {
+                    // Incorrect file
+                    return;
+                }
+            }
+        }
+        // Incorrect file
+        return;
+    }
 }
 
 // SND
@@ -89,7 +198,7 @@ void ExtractSound(Asset asset)
         // Read the sound data and convert it to 16-bit PCM
         uint num = binaryReader.ReadUInt32() + 1U;
         byte[] soundData = binaryReader.ReadBytes((int)num);
-        byte[] waveData = To16BitPCM(soundData);
+        byte[] waveData = Audio.ConvertPcm8bitTo16bit(soundData);
 
         // Set the sample rate, number of channels, and bits per sample
         int sampleRate = 22050; // Sound files are always 22050 Hz
@@ -120,36 +229,9 @@ void ExtractSound(Asset asset)
 
             // Write the audio data
             binaryWriter.Write(waveData);
+
         }
     }
-}
-
-// Convert 8-bit PCM to 16-bit PCM
-byte[] To16BitPCM(byte[] data)
-{
-    byte[] array = new byte[data.Length * 2]; // twice the length because 16-bit
-    for (int i = 0; i < data.Length; i++)
-    {
-        // This line takes the current 8-bit sample from the input array and subtracts 128 from it. 
-        // This is because 8-bit audio samples are typically signed, meaning that their range goes from -128 
-        // to +127. By subtracting 128, we center the range around 0
-
-        // The result is then shifted left by 8 bits, effectively multiplying it by 256. 
-        // This is because each 16-bit sample requires two bytes, and there are 8 bits in a byte. 
-        // So shifting left by 8 bits effectively moves the 8-bit sample to the high byte of the 16-bit sample.
-        
-        // This correctly map the range of the 8-bit sample to the larger range of the 16-bit sample  while maintaining the same center point.
-        short num = (short)(data[i] - 128 << 8); // 8-bit to 16-bit
-        // This line stores the low byte of the converted 16-bit sample in the output "array" at the current index multiplied by 2. 
-        // This is because each 16-bit sample requires two bytes, and we are storing the low byte first.
-        array[i * 2] = (byte)num; // low byte
-        // This line stores the high byte of the converted 16-bit sample in the output "array" at the current index multiplied by 2 plus 1. 
-        // This is because each 16-bit sample requires two bytes, and we are storing the high byte second.
-        // The high byte is extracted from the short "num" by shifting it to the right by 8 bits. 
-        // This effectively discards the low byte and keeps only the high byte.
-        array[i * 2 + 1] = (byte)(num >> 8); // high byte
-    }
-    return array;
 }
 
 string NormalizeFilename(Asset asset)
@@ -196,4 +278,29 @@ struct Asset
     public int length;
     public int offset;
     public string prsPath;
+}
+
+struct WaveHeader
+{
+    public ushort FormatType;      // wave type format
+    public ushort NumChannels;     // number of channels (mono/stereo)
+    public uint SampleRate;        // sample rate in Hz
+    public uint ByteRate;          // bytes per second
+    public ushort BlockAlign;      // block align
+    public ushort BitsPerSample;   // bits per sample
+    public ushort CbSize;         // unknown field with value 32 (0x20) which is a space character
+}
+
+internal struct WaveData
+{
+    public WaveHeader Header;      // wave file header
+    public ushort DataSize;        // size of remaining data in bytes
+    public ushort NumSamples;      // number of audio samples
+    public WaveSample[] Samples;   // array of audio samples
+}
+
+internal struct WaveSample
+{
+    public short LeftChannel;      // left channel audio sample
+    public short RightChannel;     // right channel audio sample (if stereo)
 }
